4d779d5cdccef36f60433865c2c869be504a5e62
devil154 <74478607+devil154@users.noreply.github.com>
2021-01-02 12:17:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from config import Config
 import logging
 
 from telegram.ext import Updater, CommandHandler
-from callbacks import startHandler, logHandler, restartHandler, adminsHandler, helpHandler, dynosHandler, aboutHandler
+from callbacks import startHandler, logHandler, restartHandler,  helpHandler, dynosHandler, aboutHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -17,14 +17,12 @@ def main():
     dispatcher = updater.dispatcher
     start_handler = CommandHandler("start",startHandler)
     help_handler = CommandHandler("help",helpHandler)
-    admins_handler = CommandHandler("admins",adminsHandler)
     log_handler = CommandHandler("log",logHandler)
     dynos_handler = CommandHandler("dynos",dynosHandler)
     restart_handler = CommandHandler("restart",restartHandler)
     about_handler = CommandHandler("about",aboutHandler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(admins_handler)
     dispatcher.add_handler(log_handler)
     dispatcher.add_handler(dynos_handler)
     dispatcher.add_handler(restart_handler)
